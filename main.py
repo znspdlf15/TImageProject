85d2ac2287a18bd686cb8b3605c04fd03e8bee27
william.lee <lyt0915@naver.com>
2019-04-03 15:21:51 +0900

    각 칸 Contour 인식

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import matplotlib.pyplot as plt
 import cv2
-from PIL import Image
-import pytesseract
 
 if __name__ == "__main__":
     image_path = r"./test_image/sudoku.png"
-    image = cv2.imread(image_path)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cropped_image = gray_image[10:45, 60:80]
-    retval, final_image = cv2.threshold(cropped_image, 100, 255, cv2.THRESH_BINARY)
-    closing = cv2.morphologyEx(cropped_image, cv2.MORPH_CLOSE, (5, 5))
-    aa = cv2.erode(closing, (5, 5), iterations=1)
+    origin_image = cv2.imread(image_path)
+    gray_image = cv2.cvtColor(origin_image, cv2.COLOR_BGR2GRAY)
+    retval, thresholding_image = cv2.threshold(gray_image, 120, 230, cv2.ADAPTIVE_THRESH_MEAN_C)
+    closing_image = cv2.morphologyEx(thresholding_image, cv2.MORPH_OPEN, (3, 3))
+    # closing_image = cv2.morphologyEx(thresholding_image, cv2.MORPH_OPEN, (3, 3))
+    # closing_image = cv2.erode(closing_image, (3, 3), iterations=1)
+    # edges = cv2.Canny(thresholding_image, 100, 200)
 
-    cv2.imshow('image', cropped_image)
-    print(pytesseract.image_to_string(cropped_image, config='--psm 6'))
+    image, contours, hierachy = cv2.findContours(closing_image.copy(),
+                                                 cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    rects = [cv2.boundingRect(each) for each in contours]
+
+    final_image = origin_image.copy()
+    # final_image = cv2.drawContours(origin_image, contours, -1, (0, 255, 0), 1)
+    for i, cnt in enumerate(contours):
+        x, y, w, h = cv2.boundingRect(cnt)
+        if h < len(final_image)/10 or w < len(final_image[0])/10:
+            continue
+        if h > len(final_image)/8 or w > len(final_image[0])/8:
+            continue
+        if i % 4 == 0:
+            cv2.rectangle(final_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        elif i % 4 == 1:
+            cv2.rectangle(final_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        elif i % 4 == 2:
+            cv2.rectangle(final_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        else:
+            cv2.rectangle(final_image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
+    # print image
+    cv2.imshow('image', final_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
